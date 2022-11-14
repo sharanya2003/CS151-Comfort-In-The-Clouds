80625984e093c593d9c2afe9401145b5ea431d6b
sharanya2003 <73152852+sharanya2003@users.noreply.github.com>
2022-11-13 16:30:36 -0800

    background attempt + hover effect on buttons

--- ComfortInThe Clouds/src/Root.java
+++ ComfortInThe Clouds/src/Root.java
@@ -1,10 +1,11 @@
+import java.io.IOException;
+
 import comfortInTheCloud.GUIdevelopment;
-import comfortInTheCloud.WelcomeImage;
 
 public class Root {
 	
-	public static void main(String[] args) {
-		WelcomeImage i = new WelcomeImage();
+	public static void main(String[] args) throws IOException {
+//		WelcomeImage i = new WelcomeImage();
 		GUIdevelopment starter = new GUIdevelopment();
 	}
 }
--- ComfortInThe Clouds/src/comfortInTheCloud/GUIdevelopment.java
+++ ComfortInThe Clouds/src/comfortInTheCloud/GUIdevelopment.java
@@ -6,17 +6,19 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 public class GUIdevelopment implements ActionListener{
 	
 	JFrame mainFrame = new JFrame("✈ ✈ ✈ Comfort In the Clouds ✈ ✈ ✈ "); //will have one frame that swiches from panel to panel
-
 	
 	//our panels
 	JPanel welcomePanel = new JPanel();
@@ -27,7 +29,6 @@ public class GUIdevelopment implements ActionListener{
 	JPanel successPanel = new JPanel();
 	
 	//contents in welcomePage
-	JLabel welcomePageBkd;
     JLabel welcomePageTitle;
     JLabel welcomePageBio1;
     JLabel welcomePageBio2;
@@ -36,28 +37,28 @@ public class GUIdevelopment implements ActionListener{
     JButton welcomePageSignUpButton;
     JButton welcomePageLoginButton;
     JLabel welcomePageFooter;
-
 	
+    ImageIcon welcomePageImage;
+	JLabel welcomePageImageLabel;
 	
 	//constructor
-	public GUIdevelopment() {
+	public GUIdevelopment() throws IOException {
 		//have welcomePage code here set up as default so when the code runs, it has a base to go off of
 		//from the welcome page, we start redirecting depending on the button clicked
 		//make sure to make the button name super clear bc each button will have diff instructions even if they do the same thing
 		//ex: welcomePageSignUpButton vs SignUpPageSubmitButton vs loginPageSubmitButton
 		//finish the variable name off with the type so its easier to detect ex if its a panel, name should have "panel" in it, if its a label, name should have "label" in it
 		
+		//trying out bckd image stuff--laggy!!
+//		welcomePageImage = new ImageIcon ("C:\\Users\\shara\\eclipse-workspace\\ComfortInTheClouds\\ComfortInThe Clouds\\bin\\comfortInTheCloud\\welcomePageBkd.jpg");
+//		welcomePageImageLabel = new JLabel();
+//		welcomePageImageLabel.setIcon(welcomePageImage);
+//		mainFrame.add(welcomePageImageLabel);
+//		mainFrame.pack();	
 		
-		//main frame setup
-		mainFrame.setSize(800,650);
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setVisible(true);
-		mainFrame.add(welcomePanel);
-
-
 		
 		//creating the welcome page
-		welcomePanel.setBackground(Color.red);
+		welcomePanel.setBackground(Color.pink);
 		welcomePanel.setLayout(null);
 		    
 		//welcome page content setup
@@ -93,30 +94,68 @@ public class GUIdevelopment implements ActionListener{
 		
 		//sign up and login button
 		welcomePageSignUpButton = new JButton("SIGN UP HERE:");
-		welcomePageSignUpButton.setBounds(125, 350, 200, 80);
+		welcomePageSignUpButton.setBounds(125, 320, 200, 80);
 		welcomePageSignUpButton.setBackground(Color.pink);
-		welcomePageSignUpButton.setForeground(Color.white);
+		welcomePageSignUpButton.setForeground(Color.black);
+		welcomePageSignUpButton.addActionListener(this);
 		welcomePanel.add(welcomePageSignUpButton);
 		
 		welcomePageLoginButton = new JButton("LOGIN HERE:");
-		welcomePageLoginButton.setBounds(425, 350, 200, 80);
+		welcomePageLoginButton.setBounds(425, 320, 200, 80);
 		welcomePageLoginButton.setBackground(Color.pink);
-		welcomePageLoginButton.setForeground(Color.white);
+		welcomePageLoginButton.setForeground(Color.black);
+		welcomePageLoginButton.addActionListener(this);
 		welcomePanel.add(welcomePageLoginButton);
 		
+		//reference from: https://stackoverflow.com/questions/22638926/how-to-put-hover-effect-on-jbutton
 		
+		//color changing when button clicked (signup)
+		welcomePageSignUpButton.addMouseListener(new java.awt.event.MouseAdapter() {
+		    public void mouseEntered(java.awt.event.MouseEvent evt) {
+		    	welcomePageSignUpButton.setBackground(Color.black);
+		    	welcomePageSignUpButton.setForeground(Color.white);
+		    }
+
+		    public void mouseExited(java.awt.event.MouseEvent evt) {
+		    	welcomePageSignUpButton.setBackground(UIManager.getColor("control"));
+		    	welcomePageSignUpButton.setBackground(Color.pink);
+		    	welcomePageSignUpButton.setForeground(Color.black);
+		    }
+		});
+		
+		//color changing when button clicked (login)
+		welcomePageLoginButton.addMouseListener(new java.awt.event.MouseAdapter() {
+				    public void mouseEntered(java.awt.event.MouseEvent evt) {
+				    	welcomePageLoginButton.setBackground(Color.black);
+				    	welcomePageLoginButton.setForeground(Color.white);
+				    }
+
+				    public void mouseExited(java.awt.event.MouseEvent evt) {
+				    	welcomePageLoginButton.setBackground(UIManager.getColor("control"));
+				    	welcomePageLoginButton.setBackground(Color.pink);
+				    	welcomePageLoginButton.setForeground(Color.black);
+				    }
+				});
 		
 		//footer
 		welcomePageFooter = new JLabel("Follow us on Twitter, Instagram, and Facebook for more updates: @ComfortInTheClouds");
-		welcomePageFooter.setFont(new Font("Monaco", Font.ROMAN_BASELINE, 18));
+		welcomePageFooter.setFont(new Font("Monaco", Font.ROMAN_BASELINE, 12));
 		welcomePageFooter.setForeground(Color.white);
-		welcomePageFooter.setBounds(40,550,900,90);
+		welcomePageFooter.setBounds(150,410,700,90);
 		welcomePanel.add(welcomePageFooter);
 		
-		
-		
+		//setting background
 		
 		mainFrame.add(welcomePanel);
+		
+		
+		
+		//main frame setup	
+		mainFrame.setSize(780,510);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
+		mainFrame.add(welcomePanel);
+		
 		//call your methods that load the page here
 		signUpPageSetUp();
 		
@@ -128,10 +167,18 @@ public class GUIdevelopment implements ActionListener{
 	public void actionPerformed(ActionEvent ae) {
 		//Organizing from welcome page to other pages!
 
+		if(ae.getSource() == this.welcomePageSignUpButton) {
+			signUpPanel.setVisible(true);
+			welcomePanel.setVisible(false); //change mypanel name to welcomepanel
+	        signUpPanel.setLayout(null);
+	        mainFrame.add(signUpPanel);
+	        mainFrame.remove(welcomePanel);
+		}
 	}
 	
 	
 	public void signUpPageSetUp() {
+		signUpPanel.setBackground(Color.pink);
 		//logic and UI stuff go here
 	}
 }

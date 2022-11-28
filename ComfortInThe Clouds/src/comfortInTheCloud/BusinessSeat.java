5ad4944a6ed9d235fdff610a9d0a24dd1a5329e4
sharanya2003 <73152852+sharanya2003@users.noreply.github.com>
2022-11-28 15:54:22 -0800

    fixed pricing error for long leg and window

--- ComfortInThe Clouds/src/comfortInTheCloud/BusinessSeat.java
+++ ComfortInThe Clouds/src/comfortInTheCloud/BusinessSeat.java
@@ -24,14 +24,14 @@ public class BusinessSeat extends Seat{
 		}
         info.append("Chosen to fly Business Class."  + "\n" + "Total price (extra costs included): $" + (getPrice() + getExtraPrice()) + "\n");
         if(this.isWindow()){
-          	info.append("You have chosen a window seat. Total flight price increased by 20 dollars" + "\n");
+          	info.append("You have chosen a window seat. Total flight price increased by 10 dollars" + "\n");
           }
           else {
           	info.append("You have not chosen a window seat. No extra charges made"+ "\n");
           }
           
           if(this.isLongLeg()) {
-          	info.append("You have chosen a long-leg seat. Total flight price increased by 10 dollars" + "\n");
+          	info.append("You have chosen a long-leg seat. Total flight price increased by 20 dollars" + "\n");
           }
           else {
           	info.append("You have not chosen a long-leg seat. No extra charges made" + "\n");
